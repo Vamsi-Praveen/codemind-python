796a17aafa8856152ba03c630f6b78acdda3fc90
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-04-29 19:18:07 +0530

    from math import sqrt def fac(x): 	count = 0 	v = [] 	while (x % 2 == 0): 		count += 1 		x = x // 2
    	if (count != 0):
    		v.append(count)
    	for i in range(3, int(sqrt(x)) + 12):
    		count = 0
    		
    		while (x % i == 0):
    			count += 1
    			x //= i
    			
    		if (count != 0):
    			v.append(count)
    	if (x > 1):
    		v.append(1)
    		
    	return v
    def nonPrimeDivisors(N):
    	
    	v = fac(N)
    	ret = 1
    	for i in range(len(v)):
    		ret = ret * (v[i] + 1)
    	ret = ret - len(v)
    	
    	return ret
    	
    N = int(input())
    print(nonPrimeDivisors(N))
    
    # This code is contributed by Samarth

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -0,0 +1,37 @@
+from math import sqrt
+def fac(x):
+	count = 0
+	v = []
+	while (x % 2 == 0):
+		count += 1
+		x = x // 2
+
+	if (count != 0):
+		v.append(count)
+	for i in range(3, int(sqrt(x)) + 12):
+		count = 0
+		
+		while (x % i == 0):
+			count += 1
+			x //= i
+			
+		if (count != 0):
+			v.append(count)
+	if (x > 1):
+		v.append(1)
+		
+	return v
+def nonPrimeDivisors(N):
+	
+	v = fac(N)
+	ret = 1
+	for i in range(len(v)):
+		ret = ret * (v[i] + 1)
+	ret = ret - len(v)
+	
+	return ret
+	
+N = int(input())
+print(nonPrimeDivisors(N))
+
+# This code is contributed by Samarth
