dbb1de2c35857ab32c53e717fd1e70d5143aa0f6
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-04-29 17:58:20 +0530

    l=int(input()) n=int(input()) len=l*l for i in range(n):     w,h=map(int,input().split())     if w<l or h<l:         print("UPLOAD ANOTHER")     elif (w>l or h>l) and (w!=h):         print('CROP IT')     elif w==h:         print('ACCEPTED')

---
+++ Roy_and_Profile_Picture.py
@@ -0,0 +1,12 @@
+l=int(input())
+n=int(input())
+len=l*l
+for i in range(n):
+    w,h=map(int,input().split())
+    if w<l or h<l:
+        print("UPLOAD ANOTHER")
+    elif (w>l or h>l) and (w!=h):
+        print('CROP IT')
+    elif w==h:
+        print('ACCEPTED')
+    
