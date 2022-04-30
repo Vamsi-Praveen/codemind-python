3902fc40868c78c5b80d796ed2842b5673f428bb
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-04-30 22:20:23 +0530

    n=int(input()) c=0 arr=list(map(int,input().strip().split()))[:n] for i in range(0,len(arr)):     if arr[i]%2==0:         p1=i         #print(i)         break arr.reverse() # print(arr) for j in range(0,len(arr)):     if arr[j]%2==0:         p2=j         #print(j)         break arr.reverse() #print(arr) for k in range(p1,len(arr)-p2-2):     if arr[k]%2==0:         c+=1 print(c)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,21 @@
+n=int(input())
+c=0
+arr=list(map(int,input().strip().split()))[:n]
+for i in range(0,len(arr)):
+    if arr[i]%2==0:
+        p1=i
+        #print(i)
+        break
+arr.reverse()
+# print(arr)
+for j in range(0,len(arr)):
+    if arr[j]%2==0:
+        p2=j
+        #print(j)
+        break
+arr.reverse()
+#print(arr)
+for k in range(p1,len(arr)-p2-2):
+    if arr[k]%2==0:
+        c+=1
+print(c)
