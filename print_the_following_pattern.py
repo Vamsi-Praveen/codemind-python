f0cabb90535bcc5d840ba7cf7916f6ed21e4ee45
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-05-01 18:52:35 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,i+1):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
-x=int(input())
-for i in range(1,x+1):
-    for j in range(1,x+1):
-        print(chr(i+64),end=' ')
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end='')
     print()
