e6430a824706fb5a6f2184ad751a6cf484a75cb8
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-05-01 13:40:45 +0530

    n=int(input()) Sum=0 v=0 arr=list(map(int,input().strip().split()))[:n] for i in range(0,len(arr)):     t=int(arr[i])     c=0     for j in range(1,t+1):         if t%j==0:             c+=1     if c==2:         v+=1         Sum=Sum+arr[i]
    avg=Sum/v
    print("%.2f"%avg)

---
+++ Average_of_primes.py
@@ -0,0 +1,17 @@
+n=int(input())
+Sum=0
+v=0
+arr=list(map(int,input().strip().split()))[:n]
+for i in range(0,len(arr)):
+    t=int(arr[i])
+    c=0
+    for j in range(1,t+1):
+        if t%j==0:
+            c+=1
+    if c==2:
+        v+=1
+        Sum=Sum+arr[i]
+            
+avg=Sum/v
+print("%.2f"%avg)
+        
