15513e843a4e3856081c803641050d6e96872fc9
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-05-01 18:50:46 +0530

    a=int(input()) for x in range(a,0,-1):     for y in range(a,0,-1):         print(y,end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,5 @@
-rows =int(input())
-
-for i in range(rows, 0, -1):
-
-    for j in range(1, i + 1):
-
-        print(j, end='')
-
-    print("")
+a=int(input())
+for x in range(a,0,-1):
+    for y in range(a,0,-1):
+        print(y,end=" ")
+    print()
