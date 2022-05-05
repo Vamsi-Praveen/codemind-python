c00ca50c6540e8a399f92fc02d2fb2dae1cf1283
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-05-05 20:29:14 +0530

    n=int(input()) k=int(input()) for i in range(n,k+1):     if i>1:         for j in range(2,int(i**0.5)+1):             if i%j==0:                 break         else:                 print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,9 @@
+n=int(input())
+k=int(input())
+for i in range(n,k+1):
+    if i>1:
+        for j in range(2,int(i**0.5)+1):
+            if i%j==0:
+                break
+        else:
+                print(i)
