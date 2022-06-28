9d569d0e20689b4199231d4ddd5dd5c6bdf35d0f
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-06-28 18:45:08 +0530

    n=int(input()) l=[] while n!=0:     d=n%10     l.append(d)     n=n//10 print(max(l))

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,11 +1,7 @@
 n=int(input())
-max=0
 l=[]
-while(n>0):
+while n!=0:
     d=n%10
     l.append(d)
     n=n//10
-for i in range(0,len(l)):
-    if l[i]>max:
-        max=l[i]
-print(max)
+print(max(l))
