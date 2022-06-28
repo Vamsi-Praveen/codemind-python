3265725423983dcb55191b379d86c6c61b962c20
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-06-28 23:09:17 +0530

    n=int(input()) arr=list(map(int,input().split()))[:n] c=0 l=[] for i in arr:     if i not in l:         l.append(i) for i in l:     if i%2==0:         c+=1 print(c)

--- unique_even_count.py
+++ unique_even_count.py
@@ -1,13 +1,12 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-v=0
+arr=list(map(int,input().split()))[:n]
+c=0
 l=[]
-for i in range(0,len(arr)):
-    c=0
-    for j in range(i+1,len(arr)):
-        if arr[i]==arr[j] and i!=j:
-            c+=1
-    if c==0:
-        if arr[i]%2==0:
-            v+=1
-print(v)
+for i in arr:
+    if i not in l:
+        l.append(i)
+for i in l:
+    if i%2==0:
+        c+=1
+print(c)
+        
