b70c36328ae64fabc0bc8e61063576802ecf3fae
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-06-28 18:25:49 +0530

    def amicable(a,b):     f1=[]     f2=[]     for i in range(1,a+1):         if a%i==0:             f1.append(i)     for i in range(1,b+1):         if b%i==0:             f2.append(i)     if sum(f1)==sum(f2):         return "Amicable"     else:         return "Not Amicable" a=int(input()) b=int(input()) print(amicable(a,b))

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,16 @@
-x=int(input())  
-y=int(input())  
-sum1=0  
-sum2=0  
-for i in range(1,x):  
-    if x%i==0:  
-        sum1+=i  
-for j in range(1,y):  
-    if y%j==0:  
-        sum2+=j  
-if(sum1==y and sum2==x):  
-    print('Amicable')  
-else:  
-    print('Not Amicable')  
+def amicable(a,b):
+    f1=[]
+    f2=[]
+    for i in range(1,a+1):
+        if a%i==0:
+            f1.append(i)
+    for i in range(1,b+1):
+        if b%i==0:
+            f2.append(i)
+    if sum(f1)==sum(f2):
+        return "Amicable"
+    else:
+        return "Not Amicable"
+a=int(input())
+b=int(input())
+print(amicable(a,b))
