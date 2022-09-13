95363473a036607323f3e3b06754775c9eb058a5
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-09-13 10:01:00 +0530

    n,m=map(int,input().split()) z={} arr=set(list(map(int,input().split()))) arr1=set(list(map(int,input().split()))) z=arr.intersection(arr1) print(len(z))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,10 +1,6 @@
 n,m=map(int,input().split())
-l=[]
-a=list(map(int,input().split()))[:n]
-b=list(map(int,input().split()))[:m]
-for i in range(0,len(a)):
-    for j in range(0,len(b)):
-        if a[i]==b[j]:
-            l.append(int(a[i]))
-l=list(set(l))
-print(len(l))
+z={}
+arr=set(list(map(int,input().split())))
+arr1=set(list(map(int,input().split())))
+z=arr.intersection(arr1)
+print(len(z))
