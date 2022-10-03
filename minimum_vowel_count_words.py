4e7373e35bd7ce06fd3347785ab440c958e3df4d
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-03 14:17:07 +0530

    n=input() c=0 d=0 s=set("aeiou") l=[] for i in n.split():     c=0     for j in i:         if j in s:             c+=1     l.append(c) x=min(l) for i in n.split():     c=0     for j in i:         if j in s:             c+=1     if c==x:         d+=1 print(d)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,20 @@
+n=input()
+c=0
+d=0
+s=set("aeiou")
+l=[]
+for i in n.split():
+    c=0
+    for j in i:
+        if j in s:
+            c+=1
+    l.append(c)
+x=min(l)
+for i in n.split():
+    c=0
+    for j in i:
+        if j in s:
+            c+=1
+    if c==x:
+        d+=1
+print(d)
