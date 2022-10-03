be77eb5fb5031b8fe37a4a91fd3ded7a30a538ab
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2022-10-03 13:41:26 +0530

    n=input().lower() c=0 vowel=set("aeiou") for i in n.split():     i=list(i)     x=len(i)-1     if i[0] in vowel and i[x] not in vowel:         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,14 +1,9 @@
-def count_words(s):
-    vowel=set("aeiouAEIOU")
-    c=0
-    for i in s.split():
-        # print(i)
-        i=list(i)
-        # print(i)
-        n=len(i)-1
-        # print(n)
-        if i[0] in vowel and i[n] not in vowel:
-            c+=1
-    return c
-s=input()
-print(count_words(s))
+n=input().lower()
+c=0
+vowel=set("aeiou")
+for i in n.split():
+    i=list(i)
+    x=len(i)-1
+    if i[0] in vowel and i[x] not in vowel:
+        c+=1
+print(c)
